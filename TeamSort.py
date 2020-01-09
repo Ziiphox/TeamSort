2f77531427acb47e5ed50337a18ed1e54418bd58
Zii <ziiphox@gmail.com>
2020-01-10 01:38:42 +1000

    Actually made the bot log in this time

---
+++ TeamSort.py
@@ -0,0 +1,21 @@
+import discord
+import json
+
+
+with open("config.json") as json_file:
+  config = json.load(json_file)
+
+
+print(f'Loading Discord.py v{discord.__version__}')
+bot = discord.Client()
+
+
+@bot.event
+async def on_ready():
+    print(f'Online as {bot.user.name}#{bot.user.discriminator}!')
+    if (bot.user.name != config["bot_name"]): #Change bot_name in config file to change bot's name on next start up. May run in to ratelimit issues if done too often.
+        print(f'Name change detected!')
+        await bot.user.edit(username=config["bot_name"])
+
+
+bot.run(config["token"])
