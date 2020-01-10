f94742047dd1d33ae1b77d563a55a5cfbd01b9c6
h4pZ <h4pz@protonmail.com>
2020-01-10 16:44:30 +0100

    Fixed it (I think)

--- cocks/help_command.py
+++ cocks/help_command.py
@@ -2,9 +2,32 @@ import discord
 from cocks import commands
 
 
-async def construct_embed():
+def get_functions(library):
+    # Getting a list of all the functions
+    # that nunchi bot supports for a
+    # message event.
+    functions = []
+
+    for attr_name in dir(library):
+        # Getting one of the objects that message_actions
+        # has declared.
+        library_object = getattr(library, attr_name)
+
+        # Checking whether is a function defined
+        # inside message_actions or if it belongs
+        # to a library that was imported.
+        if isinstance(library_object, types.FunctionType):
+            functions.append(library_object)
+
+    return functions
+
+
+def construct_embed():
     embed = discord.Embed(title="Commands help", color=0x405ecf)
-    for func_name in dir(commands):
-            func_desc = func_name.__doc__.split("\n")
-            embed.add_field(name=func_name, value=func_desc[0])
-    return embed
+
+    for function in get_functions(commands):
+        function_name = functions.__name__
+        function_description = function.__doc__.split("\n")
+        embed.add_field(name=function_name, value=function_description[0])
+
+        return embed
