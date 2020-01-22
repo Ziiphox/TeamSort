bc70cdf93c53d487278d11973fa3e6beaa35d61c
Zii <ziiphox@gmail.com>
2020-01-22 11:25:18 +1000

    function on line 66 in preferences.py returns an empty array. hapz pls help

--- cocks/commands.py
+++ cocks/commands.py
@@ -5,17 +5,23 @@ from cocks import help_command
 
 
 async def ping(msg, bot, cmd): #The message object from discord, the client object from discord, the cut up message without the prefix
-#Docstrings in functions in this file repersent what to display in the help command. First line is a very short description for when viewing all commands, second line is more descriptive and used when viewing help for a single command
+#Docstrings in functions in this file repersent what to display in the help command. 
+#First line is a very short description for when viewing all commands,
+#second line is more descriptive and used when viewing help for a single command,
+#third line is the "default permission" of the command.
     """
 Ping! Pong!
 Simply responds with Pong!
+0
     """
     await msg.channel.send("Pong!")
+    preferences.check_command_columns()
 
 async def setprefix(msg, bot, cmd):
     """
 Set my prefix
 Set the prefix you want me to listen out for. I will always respond to !help
+2
     """
     if not (len(cmd) == 2):
         await msg.channel.send("You need to set a prefix. Example: !setprefix `$`")
@@ -29,6 +35,7 @@ async def help(msg, bot, cmd):
     """
 Help command
 Shows the commands you can access. Use !help [command] to see more in-depth explination.
+0
     """
     if (len(cmd) == 2):
         await msg.channel.send(embed = help_command.find_command(cmd[1]))
--- cocks/preferences.py
+++ cocks/preferences.py
@@ -4,6 +4,8 @@ import types
 from os import path
 from cocks import commands
 
+with open("data/config.json") as json_file:
+  config = json.load(json_file)
 
 #STOLEN CODE LOL
 def get_functions(library):
@@ -28,31 +30,12 @@ def get_functions(library):
 
 
 
-if (path.exists("data/data.db")):
-    guilds = sqlite3.connect("data/data.db")
-    db = guilds.cursor()
-else:
-    guilds = sqlite3.connect("data/data.db")
-    db = guilds.cursor()
-    print("Guild data database does not exist. Creating now!")
-    db.execute('CREATE TABLE guilds (id text, prefix text, PRIMARY KEY (id) )')
-    
-    temp_query = "CREAT TABLE commands (id text"
-    for function in get_functions(commands):
-        temp_query+= f', {function.__name__} number'
-    temp_query+= ", PRIMARY KEY (id) )"
-
-    guilds.commit()
-
-with open("data/config.json") as json_file:
-  config = json.load(json_file)
-
-
 
 def get_guild_prefix(id):
     db.execute(f'SELECT prefix FROM guilds WHERE id={id}')
-    if (db.fetchone()):
-        return db.fetchone()[0]
+    prefix = db.fetchone()
+    if not (prefix == None):
+        return prefix
     else:
         return False
 
@@ -66,9 +49,40 @@ async def set_guild_prefix(id, prefix):
     guilds.commit()
 
 
-    #Add table "command_premissions"
-    #guild-ID | help | changeprefix | ect
-    #123465.. | 0    | 2            | 0-2
-    #
-    #
-    #
+#                    |
+# Admin-y type tools |
+#                    V
+
+def add_command_column(command):
+    db.execute(f'ALTER TABLE commands ADD {command} real')
+
+def remove_command_column(command):
+    db.execute(f'ALTER TABLE commands DROP COLUMN {command}')
+
+def check_command_columns():
+    db.execute('PRAGMA table_info(commands)')
+    rows = db.fetchall()
+    print(rows)
+    print(get_functions(commands))
+    for function in get_functions(commands):
+        if not (function in rows[1]):
+            print("a")
+        else:
+            print("b")
+        print("#######")
+
+
+
+
+
+if (path.exists("data/data.db")):
+    guilds = sqlite3.connect("data/data.db")
+    db = guilds.cursor()
+else:
+    guilds = sqlite3.connect("data/data.db")
+    db = guilds.cursor()
+    print("Guild data database does not exist. Creating now!")
+    db.execute('CREATE TABLE guilds (id text, prefix text, PRIMARY KEY (id) )')
+    db.execute('CREATE TABLE commands (id text, PRIMARY KEY (id) )')
+    guilds.commit()
+    check_command_columns()
